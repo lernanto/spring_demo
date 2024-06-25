b5b1e66add066e61d86b962bf164bfda9469fa24
黄艺华 <lernanto@foxmail.com>
2024-06-25 22:53:53 +0800

    add demo code

--- src/test/java/home/test_springboot/TestSpringbootApplicationTests.java
+++ src/test/java/home/test_springboot/TestSpringbootApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TestSpringbootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
